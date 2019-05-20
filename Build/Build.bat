fe579818e9692cd70ca4541ace62bc33c0d0c339
Alexander Mezhov <as.mezhov@gmail.com>
2019-05-20 13:43:26 +0500

    v2.0.5 - multitarget nuget package

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,19 +1,21 @@
-msbuild "..\GostCryptography.sln" /t:Clean /p:Configuration=Release
-msbuild "..\GostCryptography.sln" /p:Configuration=Release
+msbuild "..\GostCryptography.net40.sln" /t:Clean /p:Configuration=Release
+msbuild "..\GostCryptography.net40.sln" /p:Configuration=Release /p:FrameworkPathOverride="%PROGRAMFILES(X86)%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.2"
+msbuild "..\GostCryptography.net45.sln" /t:Clean /p:Configuration=Release
+msbuild "..\GostCryptography.net45.sln" /p:Configuration=Release
 
 if exist "..\Assemblies\Package\" rmdir /s /q "..\Assemblies\Package\"
 mkdir "..\Assemblies\Package"
 mkdir "..\Assemblies\Package\lib"
 mkdir "..\Assemblies\Package\lib\net40"
-mkdir "..\Assemblies\Package\src"
-mkdir "..\Assemblies\Package\src\Source"
-mkdir "..\Assemblies\Package\src\Source\GostCryptography"
+mkdir "..\Assemblies\Package"
+mkdir "..\Assemblies\Package\lib"
+mkdir "..\Assemblies\Package\lib\net452"
 
 copy "GostCryptography.nuspec" "..\Assemblies\Package\GostCryptography.nuspec"
-copy "..\Assemblies\GostCryptography.dll" "..\Assemblies\Package\lib\net40\GostCryptography.dll"
-copy "..\Assemblies\GostCryptography.pdb" "..\Assemblies\Package\lib\net40\GostCryptography.pdb"
-copy "..\Assemblies\GostCryptography.xml" "..\Assemblies\Package\lib\net40\GostCryptography.xml"
-xcopy "..\Source" "..\Assemblies\Package\src\Source" /E /Q /EXCLUDE:BuildExclude.txt
+copy "..\Assemblies\net40\GostCryptography.dll" "..\Assemblies\Package\lib\net40\GostCryptography.dll"
+copy "..\Assemblies\net40\GostCryptography.xml" "..\Assemblies\Package\lib\net40\GostCryptography.xml"
+copy "..\Assemblies\net45\GostCryptography.dll" "..\Assemblies\Package\lib\net452\GostCryptography.dll"
+copy "..\Assemblies\net45\GostCryptography.xml" "..\Assemblies\Package\lib\net452\GostCryptography.xml"
 
 set EnableNuGetPackageRestore=true
-"..\Tools\NuGet\NuGet.exe" pack "..\Assemblies\Package\GostCryptography.nuspec" -OutputDirectory "..\Assemblies\Package" -symbols
+"..\Tools\NuGet\NuGet.exe" pack "..\Assemblies\Package\GostCryptography.nuspec" -OutputDirectory "..\Assemblies\Package"
