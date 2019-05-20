e5742f775ddface65325b85d59df2e8507736a0a
Alexander Mezhov <as.mezhov@gmail.com>
2019-05-20 10:23:40 +0500

    v.2.0.4

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,5 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-
 msbuild "..\GostCryptography.sln" /t:Clean /p:Configuration=Release
 msbuild "..\GostCryptography.sln" /p:Configuration=Release
 
