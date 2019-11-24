c87220fb1c3c3defc927ed29d45bf58fb420c63a
Roman Buldygin <buldo@mail.ru>
2019-11-24 15:46:32 +0300

    Modern csproj with multitargeting
    * New sln file
    
    * Modern csproj with multitargeting
    
    * Updated build process

--- Source/GostCryptography/Properties/AssemblyInfo.cs
+++ Source/GostCryptography/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Security;
 
-[assembly: AssemblyTitle("GostCryptography")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GostCryptography")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("13e0930e-42fa-4821-8214-e979572a8dbe")]
 [assembly: AllowPartiallyTrustedCallers]
-[assembly: AssemblyVersion("2.0.7.0")]
-[assembly: AssemblyFileVersion("2.0.7.0")]
