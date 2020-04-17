2fe5bb7d51c3dd65f0f3aed96f5113cf9f420017
egor.okhotin <egor.okhotin@simbirsoft.com>
2020-04-17 13:33:00 +0400

    change native api & target frameworks

---
+++ Source/GostCryptography/Native/NativeApiFactory.cs
@@ -0,0 +1,12 @@
+﻿using GostCryptography.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GostCryptography.Native
+{
+    abstract class NativeApiFactory
+    {
+        public abstract INativeApi CreateApi(ProviderType provider);
+    }
+}
